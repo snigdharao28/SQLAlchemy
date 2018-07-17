5e254e7052d6c9140f8768e40cf2687967af8f1b
snigdha rao <snigdha.snigz@gmail.com>
2018-07-17 23:37:11 +0530

    user logout endpoint added

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,16 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token, 
+    jwt_refresh_token_required, 
+    get_jwt_identity
+    jwt_required,
+    get_raw_jwt
+)
 from models.user import UserModel
+from blacklist import BLACKLIST
+
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username',
@@ -64,6 +73,15 @@ class UserLogin(Resource):
             }, 200
 
         return {"message": "Invalid Credentials!"}, 401
+
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        BLACKLIST.add(jti)
+        return {'message':'successfully logged out'}, 200
+        
+
 
 class TokenRefresh(Resource):
     @jwt_refresh_token_required
