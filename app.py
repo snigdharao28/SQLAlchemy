bd464d34dae15cc89f10a985665c69d8630337bf
snigdha rao <snigdha.snigz@gmail.com>
2018-07-17 23:30:03 +0530

    blacklisting user added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
+from blacklist import BLACKLIST
 from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
@@ -16,6 +17,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'snigdha'
 api = Api(app)
 
@@ -37,6 +40,11 @@ def add_claims_to_jwt(identity):
     if identity == 1:   
         return {'is_admin': True}
     return {'is_admin': False}
+
+# This method will check if a token is blacklisted, and will be called automatically when blacklist is enabled
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST  # Here we blacklist particular users.
 
 
 # The following callbacks are used for customizing jwt response/error messages.
