1d5c4b25b1612b62a0ddd91a64d1fcacf6608f98
snigdha rao <snigdha.snigz@gmail.com>
2018-07-17 22:05:08 +0530

    login with flask_jwt_extended

--- app.py
+++ app.py
@@ -5,11 +5,10 @@
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
 from db import db
-from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -24,7 +23,9 @@ api = Api(app)
 def create_tables():
 	db.create_all()
 
-jwt = JWT(app, authenticate, identity)  # /auth
+jwt = JWTManager(app)
+
+# jwt = JWT(app, authenticate, identity)  # /auth
 
 
 api.add_resource(Store, '/store/<string:name>')
@@ -32,6 +33,8 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin,'/login')
 
 
 if __name__=='__main__':
